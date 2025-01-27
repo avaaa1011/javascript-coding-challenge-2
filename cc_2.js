bb149278cbc0185d89191def08c336a799598473
Ava Tran <ava31@usf.edu>
2025-01-26 19:30:12 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+//Task 1: Working with Arrays
+let products = ["iPhone", "iPad", "iMac", "MacBook", "AirPods"]; //declaring 5 products with an array
+products.push("Apple TV"); //adding new product to the array
+console.log(products);
+products.pop();//removed the last product
+console.log("Updated list of products: ", products);
+
