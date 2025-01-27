f9c0c2ad8dfb98cfc4d719652035bbcd560ed2a6
Ava Tran <ava31@usf.edu>
2025-01-26 19:37:34 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,12 @@ console.log(products);
 products.pop();//removed the last product
 console.log("Updated list of products: ", products);
 
+//Task 2: Accessing and Modifying Arrays
+ let products = [5, 10, 15, 20, 25]; //declaring an array scores
+ scores[1] = 11; //changed the second score in the array
+
+ let total = scores.reduce((a, b) => a + b, 0); //calculatinng the total of all the scores
+let average = total/scores.length; //calculating the average of all the scores
+console.log("Updated Scores: ", scores);
+console.log(total);
+console.log("Average Score: ", average);
